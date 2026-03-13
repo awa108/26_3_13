0a7fe80b630e92bb105fd07df421d751fdee7f36
zhaojb <3100868348@qq.com>
2026-03-13 20:19:13 +0800

    refactor

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include"math_utils.c"
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int result = factorial(n);
+    if(result == -1){
+        printf("Error: Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %d\n", n, result);
+    }
+    return 0;
+}
